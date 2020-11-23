90a4951feac438ead5e3dba826f2300b9c616ddc
15889280255 <1352176165@qq.com>
2020-11-23 08:36:22 +0800

    修改admin 音乐

--- admin/src/components/MusicBox.vue
+++ admin/src/components/MusicBox.vue
@@ -1,17 +1,47 @@
 <template>
   <el-button @click="audioClick" icon="el-icon-video-play" type="primary">
-    <audio autoplay :src="src" @click="audioClick" loop></audio>
+    <audio
+      autoplay
+      :src="src"
+      @click="audioClick"
+      loop
+      :volume="volume"
+    ></audio>
     <span v-show="!src">音乐</span>
     <span>{{ src | regName }}</span>
+    <div>
+      <el-slider
+        v-model="volume"
+        :step="1"
+        :max="100"
+        @change="volumeChange"
+        input-size="mini"
+        height="100"
+      >
+      </el-slider>
+    </div>
   </el-button>
 </template>
 
 <script>
 import { mapState } from "vuex";
 export default {
+  data() {
+    return {
+      volume: 30,
+      audio: "",
+    };
+  },
+  mounted() {
+    this.audio = this.$el.querySelector("audio");
+    this.volumeChange();
+  },
   methods: {
     audioClick() {
       this.$router.push("/system/sourceList?activeName=music");
+    },
+    volumeChange(value) {
+      this.audio.volume = this.volume / 100;
     },
   },
   computed: mapState({
@@ -28,4 +58,7 @@ export default {
 </script>
 
 <style>
+.el-slider__bar {
+  background-color: #666666;
+}
 </style>
